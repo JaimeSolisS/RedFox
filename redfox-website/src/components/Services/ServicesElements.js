8c418645d8e019be2f23b574c90db3d823cdae9c
JaimeSolisS <jaimesolissoto@me.com>
2023-04-27 23:53:19 -0600

    add services

--- redfox-website/src/components/Services/ServicesElements.js
+++ redfox-website/src/components/Services/ServicesElements.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { colors } from "../../styles/colors";
 
 export const ServicesContainer = styled.div`
   height: 800px;
@@ -6,7 +7,7 @@ export const ServicesContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background: #010606;
+  background: ${colors.darkPuple};
 
   @media screen and (max-width: 768px) {
     height: 1100px;
@@ -21,7 +22,7 @@ export const ServicesWrapper = styled.div`
   max-width: 1000px;
   margin: 0 auto;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
   align-items: center;
   grid-gap: 16px;
   padding: 0 50px;
@@ -55,8 +56,8 @@ export const ServicesCard = styled.div`
 `;
 
 export const ServicesIcon = styled.img`
-  height: 160px;
-  width: 160px;
+  max-height: 200px;
+
   margin-bottom: 10px;
 `;
 
--- redfox-website/src/components/Services/index.js
+++ redfox-website/src/components/Services/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import Icon1 from "../../images/svg-1.svg";
-import Icon2 from "../../images/svg-1.svg";
-import Icon3 from "../../images/svg-1.svg";
+import Icon1 from "../../images/image2.png";
+import Icon2 from "../../images/industrialPark.png";
+import Icon3 from "../../images/yard.jpeg";
+import Icon4 from "../../images/plant.png";
 
 import {
   ServicesContainer,
@@ -20,18 +21,19 @@ function Services() {
       <ServicesWrapper>
         <ServicesCard>
           <ServicesIcon src={Icon1} />
-          <ServicesH2>Reduce expenses</ServicesH2>
-          <ServicesP>We help reduce the fee and increase revenue.</ServicesP>
+          <ServicesH2>Ramping movements</ServicesH2>
         </ServicesCard>
         <ServicesCard>
           <ServicesIcon src={Icon2} />
-          <ServicesH2>Virtual Offices</ServicesH2>
-          <ServicesP>We help reduce the fee and increase revenue.</ServicesP>
+          <ServicesH2>Transfers within the industrial park</ServicesH2>
         </ServicesCard>
         <ServicesCard>
           <ServicesIcon src={Icon3} />
-          <ServicesH2>Premium Benefits</ServicesH2>
-          <ServicesP>We help reduce the fee and increase revenue.</ServicesP>
+          <ServicesH2>Reorganization of boxes within yards</ServicesH2>
+        </ServicesCard>
+        <ServicesCard>
+          <ServicesIcon src={Icon4} />
+          <ServicesH2>Movements within plants</ServicesH2>
         </ServicesCard>
       </ServicesWrapper>
     </ServicesContainer>
