04580f1123fb857541a99cd77280ae406832362a
JaimeSolisS <jaimesolissoto@me.com>
2023-05-01 17:50:02 -0600

    add new colors

--- redfox-website/src/styles/colors.js
+++ redfox-website/src/styles/colors.js
@@ -4,5 +4,10 @@ export const colors = {
     whatsapp: "#01bf71",
     lightPurple: "#fb8104",
     darkPuple: "#fb8104",
+    lightOrange: "#fb8104",
+    darkOrange: "#d86F04",
+    burntOrange: "#c66705",
+    lightGray: "#757780",
+    darkGray: "#545454",
     red: "#757780",
 };
