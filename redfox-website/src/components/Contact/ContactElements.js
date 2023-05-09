13fae15f5413cac862604454e110112866500280
JaimeSolisS <jaimesolissoto@me.com>
2023-05-08 18:04:49 -0600

    add Contact Section

---
+++ redfox-website/src/components/Contact/ContactElements.js
@@ -0,0 +1,154 @@
+import styled from "styled-components";
+import { colors } from "../../styles/colors";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const ContactContainer = styled.div`
+  height: 800px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and (max-width: 768px) {
+    padding: 6.25rem 0;
+  }
+`;
+
+export const ContactWrapper = styled.div`
+  display: grid;
+  z-index: 1;
+  height: 650px;
+  width: 100%;
+  max-width: 1100px;
+  margin-right: auto;
+  margin-left: auto;
+  padding: 0 24px;
+  justify-content: center;
+  grid-template-columns: 1fr; // add this property
+`;
+
+export const ContactRow = styled.div`
+  display: flex;
+  width: 100%;
+  display: grid;
+
+  //grid-auto-columns: repeat(2, 1fr);
+  align-items: center;
+  grid-template-areas: ${({ imgStart }) =>
+    imgStart ? `'col2 col1'` : ` 'col1 col2'`};
+  grid-template-columns: 30% 70%;
+  @media screen and (max-width: 768px) {
+    grid-template-areas: ${({ imgStart }) =>
+      imgStart ? `'col2 col2''col1 col1'` : `'col1 col1''col2 col2'`};
+  }
+`;
+
+export const GridWrapper = styled.div`
+  width: 100%;
+  margin-bottom: 15px;
+  padding: 0 15px;
+`;
+
+export const Column1 = styled(GridWrapper)`
+  grid-area: col1;
+`;
+
+export const Column2 = styled(GridWrapper)`
+  grid-area: col2;
+`;
+
+export const ContactH1 = styled.h1`
+  font-size: 2.5rem;
+  color: #fff;
+  margin-bottom: 64px;
+
+  @media screen and (max-width: 480px) {
+    font-size: 2rem;
+  }
+`;
+
+export const InformationContainer = styled.div`
+  background: red;
+  align-items: center;
+  padding: 16px;
+  margin-bottom: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+export const ContactH2 = styled.h2`
+  font-size: 1.5rem;
+  color: #fff;
+  margin-bottom: 64px;
+
+  @media screen and (max-width: 480px) {
+    font-size: 2rem;
+  }
+`;
+
+export const DataLine = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const ContactIcon = styled(FontAwesomeIcon)`
+  color: ${colors.white};
+  font-size: 2.5rem;
+  margin-bottom: 10px;
+`;
+
+export const Title = styled.h3`
+  font-size: 20px;
+  margin-bottom: 8px;
+`;
+
+export const DataText = styled.p`
+  font-size: 16px;
+  color: ${colors.white};
+`;
+
+export const StyledContactForm = styled.div`
+  form {
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    width: 100%;
+    font-size: 16px;
+    input {
+      width: 100%;
+      height: 35px;
+      padding: 7px;
+      outline: none;
+      border-radius: 5px;
+      border: 1px solid rgb(220, 220, 220);
+      &:focus {
+        border: 2px solid rgba(0, 206, 158, 1);
+      }
+    }
+    textarea {
+      max-width: 100%;
+      min-width: 100%;
+      width: 100%;
+      max-height: 100px;
+      min-height: 100px;
+      padding: 7px;
+      outline: none;
+      border-radius: 5px;
+      border: 1px solid rgb(220, 220, 220);
+      &:focus {
+        border: 2px solid rgba(0, 206, 158, 1);
+      }
+    }
+    label {
+      margin-top: 1rem;
+    }
+    input[type="submit"] {
+      margin-top: 2rem;
+      cursor: pointer;
+      background: rgb(249, 105, 14);
+      color: white;
+      border: none;
+    }
+  }
+`;
---
+++ redfox-website/src/components/Contact/index.js
@@ -0,0 +1,93 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import {
+  ContactContainer,
+  ContactH1,
+  StyledContactForm,
+  ContactWrapper,
+  Column1,
+  Column2,
+  ContactRow,
+  InformationContainer,
+  ContactH2,
+  DataLine,
+  DataText,
+  ContactIcon,
+  GridWrapper,
+} from "./ContactElements";
+import {
+  faPhone,
+  faEnvelope,
+  faMapMarkerAlt,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Contact = ({ imgStart }) => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "replace with service id",
+        "replace with template id",
+        form.current,
+        "replace with user id"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+          e.target.reset();
+        }
+      );
+  };
+
+  return (
+    <ContactContainer id="contact">
+      <ContactH1>Contact Us</ContactH1>
+      <ContactWrapper>
+        <ContactRow imgStart={imgStart}>
+          <Column1>
+            <InformationContainer>
+              <ContactH2>Contact Information</ContactH2>
+              <DataLine>
+                <ContactIcon icon={faPhone} />
+                <DataText>915-300-3571</DataText>
+              </DataLine>
+              <DataLine>
+                <ContactIcon icon={faEnvelope} />
+                <DataText>admin@rfintlogistics.com</DataText>
+              </DataLine>
+              <DataLine>
+                <ContactIcon icon={faMapMarkerAlt} />
+                <DataText>
+                  1525 Wyoming Ave <br /> El Paso, TX 79902
+                </DataText>
+              </DataLine>
+            </InformationContainer>
+          </Column1>
+          <Column2>
+            <StyledContactForm>
+              <form ref={form} onSubmit={sendEmail}>
+                <label>Name</label>
+                <input type="text" name="user_name" />
+                <label>Email</label>
+                <input type="email" name="user_email" />
+                <label>Message</label>
+                <textarea name="message" />
+                <input type="submit" value="Send" />
+              </form>
+            </StyledContactForm>
+          </Column2>
+        </ContactRow>
+      </ContactWrapper>
+    </ContactContainer>
+  );
+};
+
+export default Contact;
